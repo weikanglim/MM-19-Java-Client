d21603a7449f3451c24ed6074d394fdb8998ee17
Your Name <you@example.com>
2013-10-12 13:46:33 -0500

    Added random name

--- run.sh
+++ run.sh
@@ -1,2 +1,56 @@
 #!/bin/bash
-java -cp "bin/" mm19.runner.TestClientRunner
+
+list=(
+"Rosa Darkmoon",
+"Joe Dog Stoker",
+"Salty Simon Bloodrayne",
+"Gold Thomas Digger",
+"Bloody Edward Blacksmith",
+"Doubloon Thomas Digger",
+"Captain Rosa Silverblade",
+"Salty Johnny Bloodrayne",
+"Morbid James Darkblade",
+"Captain Jack Harker",
+"Joe Black Harker",
+"Scarlet George Bloodrayne",
+"Captain John Bloodrayne",
+"Swag Scarlet Bloodrayne",
+"Nathan Blimey Ravenblack",
+"Anne Canon Hawkins",
+"Bloody Thomas Redbeard",
+"Stormy James Blackman",
+"Salty Johnny Gully",
+"Salty Katja Davis",
+"Deadlights Simon Stoker",
+"Francois Scully",
+"John Ravenbeard",
+"Gruesome Johnny Bloodrayne",
+"George Dangerous Sangre",
+"Captain Marcia Hawkins",
+"Henri Bloody Redbeard",
+"Captain Peter Grimbeard",
+"Jacob Brutal Shelley",
+"Devil Bill Dreadbeard",
+"Bloody Simon Bloodrayne",
+"Silver Steve Gully",
+"Stormy Billy Grimbeard",
+"Walker Silver Blackman",
+"Captain John Ravenblack",
+"Steve Blacksmith",
+"Johnny Gold Ravenblack",
+"Vicious Jacob Redbeard",
+"George Blackstroker",
+"Tessa Crawhawk",
+"Killer Brutus Crawhawk",
+"Bill Dangerous Black",
+"Captain James Fargloom",
+"Evil John Blackman",
+"Nathan Barbaric Hawkins",
+"Hungry Steve Blackstroker",
+"Roger Gray Fargloom",
+"Shanty Nathan Deadwood"
+)
+
+i=$(( $RANDOM % 48 ))
+
+java -cp "bin/" mm19.runner.TestClientRunner  "${list[$i]}"
